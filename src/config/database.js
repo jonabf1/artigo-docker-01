d920e0669d8a19977f7c70df99df3f37462123e1
Jonathan Barros <jonabf1@gmail.com>
2019-12-18 01:27:16 -0300

    fix bug

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 
 module.exports = {
-  dialect: 'postgres',
+  dialect:  'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
